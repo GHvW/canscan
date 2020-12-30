75ac42e019a972622307fa0e2a84ac48b03a4769
Garrett van Wageningen <ghvanw283@gmail.com>
2020-12-29 22:52:13 -0600

    save work

--- src/canscan.cr
+++ src/canscan.cr
@@ -43,23 +43,88 @@ module Canscan
   # end
 
   # 4 better, but will show inconsistent results, with spawn macro
-  done_chan = Channel(Nil).new
+  # done_chan = Channel(Nil).new
 
-  address = "scanme.nmap.org"
-  (1..1024).each do |i|
-    # puts "scanning #{address}:#{i}"
-    spawn ->(n : Int32) {
+  # address = "scanme.nmap.org"
+  # (1..1024).each do |i|
+  #   # puts "scanning #{address}:#{i}"
+  #   spawn ->(n : Int32) {
+  #     begin
+  #       TCPSocket.open(address, i) do |conn|
+  #         puts "#{address}:#{i} is open"
+  #       end
+  #     rescue
+  #     end
+  #     done_chan.send(nil)
+  #   }.call(i)
+  # end
+
+  # (1..1024).each do # how we wait since we dont have a WaitGroup like Go
+  #   done_chan.receive
+  # end
+
+  # 5, this one didn't really work
+  # def self.worker(ports, done)
+  #   while !ports.closed?
+  #     puts ports.receive
+  #     done.send(true)
+  #   end
+  # end
+
+  # ports_chan = Channel(Int32).new(100) # pool of 100 workers
+  # done_chan = Channel(Bool).new
+
+  # 100.times do
+  #   spawn worker(ports_chan, done_chan)
+  # end
+
+  # spawn do
+  #   (1..1024).each do |i|
+  #     ports_chan.send(i)
+  #   end
+  # end
+
+  # 1024.times do
+  #   done_chan.receive
+  # end
+
+  # ports_chan.close
+
+  # 6 
+  def self.worker(ports, results)
+    address = "scanme.nmap.org"
+    while !ports.closed?
+      i = ports.receive
       begin
         TCPSocket.open(address, i) do |conn|
           puts "#{address}:#{i} is open"
         end
       rescue
       end
-      done_chan.send(nil)
-    }.call(i)
+
+      done.send(true)
+    end
   end
 
-  (1..1024).each do # how we wait since we dont have a WaitGroup like Go
-    done_chan.receive
+  ports_chan = Channel(Int32).new(100) # pool of 100 workers
+  results_chan = Channel(Int32).new
+
+  100.times do
+    spawn worker(ports_chan, results_chan)
   end
+
+  spawn do
+    (1..1024).each do |i|
+      ports_chan.send(i)
+    end
+  end
+
+  1024.times do
+    results_chan.receive
+  end
+
+  ports_chan.close
+  results_chan.close
+
+  puts "Bye Bye"
 end
